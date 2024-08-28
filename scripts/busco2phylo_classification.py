55105e4e2f5ab0f26160f2c2e0812913fbdf2d51
KamilSJaron <kamiljaron@gmail.com>
2024-08-28 16:53:21 +0200

    drafting the per gene summary 2

--- scripts/busco2phylo_classification.py
+++ scripts/busco2phylo_classification.py
@@ -11,17 +11,20 @@ cecidomyiidae = set(['Sitodiplosis mosellana', 'Mayetiola destructor', 'Obolodip
 
 ############ FUNCTIONS
 
+def is_grc(tip):
+    return(tip.name.startswith('L-'))
+
 def is_just_grcs(clade):
-    return(all([tip.name.startswith('L-') for tip in clade.get_terminals()]))
+    return(all([is_grc(tip) for tip in clade.get_terminals()]))
 
 def is_monophyletic_sciaridae(clade):
     return(all([tip2species_name(tip) in sciaridae for tip in clade.get_terminals()]))
 
 def is_monophyletic_cecidomyiidae(clade):
-    return(all([tip2species_name(tip) in cecidomyiidae or tip.name.startswith('L-') for tip in clade.get_terminals()]))
+    return(all([tip2species_name(tip) in cecidomyiidae or is_grc(tip) for tip in clade.get_terminals()]))
 
 def count_grcs(clade):
-    return(sum([tip.name.startswith('L-') for tip in clade.get_terminals()]))
+    return(sum([is_grc(tip) for tip in clade.get_terminals()]))
 
 def tip2species_name(tip):
     sp_string = ' '.join(tip.name.split('_')[0:2])
@@ -34,6 +37,18 @@ def tip2short_name(tip):
     name_in_sections = tip2species_name(tip).split(' ')
     return(name_in_sections[0][0] + name_in_sections[1][0:3])
 
+def grc_tip2sp_scf_loc(tip):
+    sp = tip2short_name(tip)
+    if not "|" in tip.name:
+        tipname_split = tip.name.split('_')
+        chrom = tipname_split[2]
+        loc = tipname_split[3]
+    else:
+        tipname_split = tip.name.split('|')[1].split('_')
+        chrom = '_'.join(tipname_split[0:2])
+        loc = tipname_split[2]
+    return(sp, chrom, loc)
+
 ######### file manipulation bit
 # input_dir = sys.argv[1]
 input_dir = 'data/testing_trees_busco'
@@ -42,11 +57,12 @@ tree_files = [i for i in os.listdir(input_dir) if i.endswith('treefile')]
 # tree_summary = 'summary_all_trees.tsv'
 # GRC_genes_summary = 'phylogenetic_placement_of_all_grc_genes.tsv'
 
-# tree_file = tree_files[0]
+# tree_file = tree_files[1]
 # trees = [Phylo.read(input_dir + '/' + tree_file, "newick") for tree_file in tree_files]
-# tree = trees[0]
+# tree = trees[1]
 # Phylo.draw(tree)
 
+######### Per TREE summary
 row_to_print = 'BUSCO_id\ttotal_genes\tmonophyletic_sci\tmonophyletic_ceci\tGRC_species\tGRCs_total\tGRCs_sci\tGRCs_ceci\n)'
 sys.stdout.write(row_to_print)
 
@@ -64,14 +80,14 @@ for tree_file in tree_files:
     for tip in tree.get_terminals():
         sp_name = tip2species_name(tip)
         # sp2tips[sp_name].append(tip)
-        if tip.name.startswith('L-'):
+        if is_grc(tip):
             present_GRCs.add(tip)
         elif sp_name in sciaridae:
             present_sciaridae.add(tip)
         if sp_name in cecidomyiidae:
             present_cecidomyiidae.add(tip)
-        
-    ### testing
+            
+    ######### Per Gene summary
     monophyletic_sci = is_monophyletic_sciaridae(tree.common_ancestor(present_sciaridae)) ## 3
     monophyletic_ceci = is_monophyletic_cecidomyiidae(tree.common_ancestor(present_cecidomyiidae)) ## 4
 
@@ -83,9 +99,34 @@ for tree_file in tree_files:
     row_to_print = '\t'.join([BUSCO_id, total_tips, str(monophyletic_sci), str(monophyletic_ceci), GRC_species, GRCs_total, GRCs_sci, GRCs_ceci]) + '\n'
     sys.stdout.write(row_to_print)
 
+    ######### Per branch summary
+    row_to_print = 'BUSCO_id\tsp\tchromosome\tlocation\tGRC_closest_relative\tGRC_bootstrap\tnon-GRC_closest_relative\tnon-GRC_bootstrap\tbranch_length\n)'
+    sys.stdout.write(row_to_print)
 
-# cecido_branch = tree.common_ancestor(sp2tips['Mayetiola destructor'] + sp2tips['Resseliella maxima'])
-# is_just_grcs(cecido_branch)
-# is_monophyletic_sciaridae(cecido_branch)
-# is_monophyletic_cecidomyiidae(cecido_branch)
-# Ok, all the code seems to behave in the way I would expect it to
+    for grc in present_GRCs:
+        sp, chrom, loc = grc_tip2sp_scf_loc(grc) ### 2,3,4 of the reported values: species, chromsome, location
+        grc2root = list(reversed(tree.get_path(grc)[:-1])) # I am storing the whole path from root to the investigated termial branch (excluding that branch, thjat's [:-1] which removes the last value)
+        non_grc_bootstraps = []
+        grc_bootstraps = []
+
+        for target_node in grc2root: # target_node is is the last common ancestor (node) of the GRC gene and any non-GRC gene
+            non_grc_bootstraps.append(target_node.name) # this will record the whole line of bootstraps
+            if not is_just_grcs(target_node): # breaks on the fist node with non-GRC ancestors OR at the root
+                break
+        non_grc_ancestor = target_node 
+        closest_non_grc_relatives = ';'.join([tip2short_name(tip) for tip in non_grc_ancestor.get_terminals() if not is_grc(tip)]) ### 7
+        closest_non_grc_relatives_bs = ';'.join(non_grc_bootstraps) ### 8
+
+        for target_node in grc2root: # target_node is is the last common ancestor (node) of the GRC gene and any other GRC gene
+            grc_bootstraps.append(target_node.name) # this will record the whole line of bootstraps, necesarily will overlap with the others
+            if is_just_grcs(target_node): # breaks on the fist node with GRC ancestors OR at the root
+                break
+        grc_ancestor = target_node 
+        closest_grc_relatives = ';'.join([tip2short_name(tip) for tip in grc_ancestor.get_terminals() if is_grc(tip) and not tip == grc]) ### 5
+        closest_grc_relatives_bs = ';'.join(grc_bootstraps) ### 6
+        
+        row_to_print = '\t'.join([BUSCO_id, sp, chrom, loc, closest_grc_relatives, closest_grc_relatives_bs, closest_non_grc_relatives, closest_non_grc_relatives_bs, str(grc.branch_length)]) + '\n'
+        sys.stdout.write(row_to_print)
+
+        # Phylo.write(non_grc_ancestor, sys.stdout, "newick") # it get's messy with Newick
+
