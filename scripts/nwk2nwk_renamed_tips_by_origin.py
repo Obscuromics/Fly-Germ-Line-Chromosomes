005cbb94161ce2f83f7cd02668ed4b1add0ce9c7
KamilSJaron <kamiljaron@gmail.com>
2025-09-09 08:49:18 +0200

    adding renaming script

---
+++ scripts/nwk2nwk_renamed_tips_by_origin.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+from os import listdir
+from os import path
+from os import makedirs
+from Bio import Phylo
+from sys import argv
+from sys import stderr
+
+input_dir = argv[1]
+# input_dir = 'data/testing_trees_busco/'
+ouput_dir = argv[2]
+# ouput_dir = 'data/testing_trees_busco_renamed/'
+busco_table_filename = argv[3]
+# busco_table_filename = 'tables/busco_grc_classification_odb10_diptera.tsv'
+
+
+if not path.exists(ouput_dir):
+    stderr.write("Creating:" + ouput_dir + '\n')
+    makedirs(ouput_dir)
+
+origin_dict = defaultdict(lambda: 'NA')
+which_GRC_dict = defaultdict(lambda: 'Core')
+
+with open(busco_table_filename) as f:
+    for line in f:
+        BUSCO_line = line.strip().split()
+        if BUSCO_line[0] == 'BUSCO_id':
+            continue
+        key = BUSCO_line[1].split('_')[0] + BUSCO_line[0]
+        # sp_busco e.g. Ling_40951at7147
+        which_GRC_dict[key] = BUSCO_line[1].split('_')[2]
+        if BUSCO_line[4] == "Cecidomyiidae":
+            origin_dict[key] = 'c'
+        else:
+            origin_dict[key] = 's'
+
+stderr.write("Extracted " + str(len(origin_dict.keys())) + ' busco + species -> origin records\n')
+
+tree_files = [i for i in listdir(input_dir) if i.endswith('treefile')]
+target_species = ['Bradysia_coprophila', 'Bradysia_impatiens', 'Lycoriella_ingenua']
+
+stderr.write("Found: " + str(len(tree_files)) + ' trees in' + input_dir + '\n')
+stderr.write('Output trees will be written in' + ouput_dir + '\n')
+
+for tree_file in tree_files:
+    busco = tree_file.rstrip('.treefile')
+    input_newick = input_dir + '/' + tree_file
+    output_newick = ouput_dir + '/' + tree_file
+    tree = Phylo.read(input_newick, "newick")
+    for i in range(len(tree.get_terminals())):
+        terminal_orig_name = tree.get_terminals()[i].name
+        species = "_".join(terminal_orig_name.split('_')[:2])
+        if '-' in species:
+            species = species.split('-')[1]
+        if species in target_species:
+            sp_short = species.split("_")[0][0] + species.split("_")[1][0:3]
+            dict_key = sp_short + busco
+            new_tip_name = which_GRC_dict[dict_key] + '-' + origin_dict[dict_key] + '-' + species
+        else:
+            new_tip_name = species
+        tree.get_terminals()[i].name = new_tip_name
+        Phylo.write(tree, output_newick, "newick")
+
+stderr.write('All done\n')
